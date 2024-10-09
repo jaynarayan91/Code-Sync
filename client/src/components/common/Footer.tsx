201fc8d7fa7e08afe050cf2a0ac3794f64e16134
Jay Narayan <sharma95084@gmail.com>
2024-10-10 00:45:15 +0530

    first commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" Jay Narayan "}
+                <a
+                    href="https://github.com/jaynarayan91"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Jay Narayan
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
